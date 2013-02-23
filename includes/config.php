42e378495109a8477a31169fd62e53d317107d56
Tamara <tamagochan99@gmail.com>
2013-02-23 13:47:04 -0300

    initial commit
    just file structure and config file

---
+++ includes/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$config = array(
+		"db" => array(
+				"default" => array(
+						"dbname" => "",
+						"username" => "",
+						"password" => "",
+						"host" => ""
+						)
+				)
+		);
+
+/*
+ Error reporting.
+*/
+ini_set("error_reporting", "true");
+error_reporting(E_ALL|E_STRCT);
